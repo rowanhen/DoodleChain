330b1e69b189c60ab23caa738726c03227d41d46
rowanhen <rowan.henseleit@marshmallow.co>
2024-01-01 18:35:15 +0000

    added sepolia connection

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,20 @@
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.19",
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${SEPOLIA_PRIVATE_KEY}`],
     },
   },
 };
